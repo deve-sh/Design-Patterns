86ce9f9f3eff801efe2a57b663da2a3a0a2d4922
Devesh Kumar <devesh2027@gmail.com>
2021-08-27 14:59:00 +0530

    Added minor description for Abstract Factory Pattern

---
+++ Creational/AbstractFactoryPattern.ts
@@ -0,0 +1,6 @@
+/**
+ * This Design Pattern is very similar to the Factory Design Pattern
+ * 
+ * But the only difference is Abstract Factory Pattern returns Factories that generate Objects.
+ * 
+ */
