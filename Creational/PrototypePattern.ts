f528558c4a67e868f180f6e64875db8d1d6d2eb3
Devesh Kumar <devesh2027@gmail.com>
2021-08-29 21:53:08 +0530

    Added explanation comments to Prototype Pattern

---
+++ Creational/PrototypePattern.ts
@@ -0,0 +1,9 @@
+/**
+ * The protoype pattern is especially useful when you want to create an exact replica of an existing object.
+ * It reduces the boilerplate involved in copying an object to another.
+ * 
+ * References:
+ * -----------
+ * https://www.dofactory.com/javascript/design-patterns/prototype
+ * https://refactoring.guru/design-patterns/prototype
+ */
